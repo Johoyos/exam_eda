e8431cba9fb74607baa065134235114ac38b3f61
Roosevelt Jhans Ubaldo Chavez <roosevelt.ubaldo@outlook.com>
2020-11-14 14:40:27 -0500

    fixed lenght record

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "record.h"
+using namespace std;
+
+int main (){
+insertRecords();
+}
---
+++ record.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <cstring>
+
+struct Record{
+    char key[4];
+	char value[3000];
+};
+
+void insertRecords(){
+	std::ifstream infile("dataset.txt");
+	system("rm data.db");
+	std::ofstream outfile("data.db", std::ios::binary | std::ios::app);
+	std::string line;
+	while (std::getline(infile, line)){
+		Record r;
+        std::string key(line.begin(), line.begin()+4);
+        std::string value(line.begin()+4, line.end());
+		strcpy(r.key, key.c_str());
+        strcpy(r.value, value.c_str());
+	    outfile.write((char*)&r, sizeof(Record));
+	}
+	infile.close();
+	outfile.close();
+	//system("rm files.txt");
+};
